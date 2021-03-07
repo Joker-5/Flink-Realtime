2e95e0f86de3374d99d0120958ebbac5c3cfab04
Joker-5 <2725284321@qq.com>
2021-03-07 13:11:52 +0800

    realtime

---
+++ gmall2021-logger/src/main/java/com/joker/gmall/controller/LoggerController.java
@@ -0,0 +1,29 @@
+package com.joker.gmall.controller;/*
+ *项目名: gmall2021-parent
+ *文件名: LoggerController
+ *创建者: Joker
+ *创建时间:2021/3/3 23:33
+ *描述:
+
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LoggerController {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping("/applog")
+    public String getLogger(@RequestParam("param") String jsonStr) {
+        log.info(jsonStr);
+        kafkaTemplate.send("ods_base_log", jsonStr);
+        return "success";
+    }
+}
---
+++ gmall2021-realtime/src/main/java/com/joker/gmall/realtime/utils/MyKafkaUtil.java
@@ -0,0 +1,12 @@
+package com.joker.gmall.realtime.utils;/*
+ *项目名: gmall2021-parent
+ *文件名: MyKafkaUtil
+ *创建者: Joker
+ *创建时间:2021/3/7 13:10
+ *描述:
+
+ */
+
+public class MyKafkaUtil {
+
+}
