cfe1065e8ec3ac6891ee9e819c122dfa097c8188
Joker-5 <2725284321@qq.com>
2021-03-13 11:03:51 +0800

    DWD层构建完成

--- gmall2021-realtime/src/main/java/com/joker/gmall/realtime/app/func/TableProcessFunction.java
+++ gmall2021-realtime/src/main/java/com/joker/gmall/realtime/app/func/TableProcessFunction.java
@@ -9,15 +9,15 @@ package com.joker.gmall.realtime.app.func;/*
 
 import com.alibaba.fastjson.JSONObject;
 import com.joker.gmall.realtime.bean.TableProcess;
+import com.joker.gmall.realtime.common.constant.GmallConfig;
 import com.joker.gmall.realtime.utils.MyMysqlUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.*;
 
 
@@ -39,8 +39,8 @@ public class TableProcessFunction extends ProcessFunction<JSONObject, JSONObject
     public void open(Configuration parameters) throws Exception {
         //初始化phoenix连接
         Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
-        //TODO phoenix连接配置
-        connection = DriverManager.getConnection("TODO");
+        //phoenix连接配置
+        connection = DriverManager.getConnection(GmallConfig.PHOENIX_SERVER);
         //定时任务，每5s去mysql更新一次配置信息
         Timer timer = new Timer();
         timer.schedule(
@@ -53,9 +53,9 @@ public class TableProcessFunction extends ProcessFunction<JSONObject, JSONObject
                 , 5000, 5000);
     }
 
-    //读取mysql配置表信息存入内存中
+    //加载配置信息->读取mysql配置表信息存入内存中
     private void initProcessTableMap() {
-        System.out.println("更新读取的配置信息~~~");
+        //System.out.println("更新读取的配置信息~~~");
         List<TableProcess> tableProcessList = MyMysqlUtil.query("select * from table_process", TableProcess.class, true);
         for (TableProcess process : tableProcessList) {
             //依次读取配置信息
@@ -100,13 +100,82 @@ public class TableProcessFunction extends ProcessFunction<JSONObject, JSONObject
         if (ext == null) {
             ext = "";
         }
-        StringBuilder sql = new StringBuilder("create table if not exists ");
-        //TODO 拼接sql
+        StringBuilder sql = new StringBuilder("create table if not exists " + GmallConfig.HBASE_SCHEMA
+                + "." + tableName + "(");
+        //将列做切分,并拼接至建表语句 SQL 中
+        String[] fieldsArr = fields.split(",");
+        for (int i = 0; i < fieldsArr.length; i++) {
+            String field = fieldsArr[i];
+            if (pk.equals(field)) {
+                sql.append(field).append(" varchar primary key ");
+            } else {
+                sql.append("info.").append(field).append(" varchar");
+            }
+            if (i < fieldsArr.length - 1) {
+                sql.append(",");
+            }
+        }
+        sql.append(")");
+        sql.append(ext);
+        try {
+            //执行建表语句在 Phoenix 中创建表
+            System.out.println(sql);
+            PreparedStatement ps = connection.prepareStatement(sql.toString());
+            ps.execute();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("建表失败！！！");
+        }
     }
+
 
     //每个元素进行的处理
     @Override
-    public void processElement(JSONObject jsonObject, Context context, Collector<JSONObject> collector) throws Exception {
+    public void processElement(JSONObject jsonObj, Context ctx, Collector<JSONObject> collector) throws Exception {
+        String table = jsonObj.getString("table");
+        String type = jsonObj.getString("type");
+        // 坑！！如果是使用 Maxwell 的初始化功能，他会将mysql之前的数据读取进来
+        // 那么 type 类型为 bootstrap-insert
+        // 将这里也标记为 insert,不然可能后续运行时会报错
+        if (type.equals("bootstrap-insert")) {
+            type = "insert";
+            jsonObj.put("type", type);
+        }
+        //获取配置表信息
+        if (tableProcessMap != null && tableProcessMap.size() != 0) {
+            //注意主键格式要和上面设置的一样
+            String key = table + ":" + type;
+            TableProcess tableProcess = tableProcessMap.get(key);
+            if (tableProcess != null) {
+                //业务数据进一步处理
+                jsonObj.put("sink_table", tableProcess.getSinkTable());
+                String sinkColumns = tableProcess.getSinkColumns();
+                if (sinkColumns != null && sinkColumns.length() > 0) {
+                    //对业务数据json data字段数据进行过滤
+                    filterColumn(jsonObj.getJSONObject("data"), sinkColumns);
+                } else {
+                    System.out.println("No this key : " + key);
+                }
+            }
+            //hbase维度数据输入到侧输出流
+            //System.out.println("开始分流~~~~");
+            if (tableProcess != null && TableProcess.SINK_TYPE_HBASE.equalsIgnoreCase(tableProcess.getSinkType())) {
+                ctx.output(outputTag, jsonObj);
+                //主流事实表数据输出到kafka
+            } else if (tableProcess != null && TableProcess.SINK_TYPE_KAFKA.equalsIgnoreCase(tableProcess.getSinkType())) {
+                collector.collect(jsonObj);
+            }
+        }
 
+    }
+
+    //过滤JSON中的多余字段
+    private void filterColumn(JSONObject data, String sinkColumns) {
+        String[] cols = StringUtils.split(sinkColumns, ",");
+        Set<Map.Entry<String, Object>> entries = data.entrySet();
+        List<String> columnList = Arrays.asList(cols);
+        //精简写法删除元素
+        entries.removeIf(entry -> !columnList.contains(entry.getKey()));
     }
 }
--- gmall2021-realtime/src/main/java/com/joker/gmall/realtime/utils/MyKafkaUtil.java
+++ gmall2021-realtime/src/main/java/com/joker/gmall/realtime/utils/MyKafkaUtil.java
@@ -10,13 +10,16 @@ package com.joker.gmall.realtime.utils;/*
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 
 import java.util.Properties;
 
 public class MyKafkaUtil {
 
     private static final String KAFKA_SERVER = "hadoop202:9092,hadoop203:9092,hadoop204:9092";
+    private static final String DEFAULT_TOPIC = "DEFAULT_DATA";
 
     //封装kafka消费者
     public static FlinkKafkaConsumer<String> getKafkaSource(String topic, String groupId) {
@@ -30,4 +33,14 @@ public class MyKafkaUtil {
     public static FlinkKafkaProducer<String> getKafkaSink(String topic) {
         return new FlinkKafkaProducer<>(KAFKA_SERVER, topic, new SimpleStringSchema());
     }
+
+    //封装kafka生产者,使其动态地向不同topic发送数据并序列化
+    public static <T> FlinkKafkaProducer<T> getKafkaSinkBySchema(KafkaSerializationSchema<T> serializationSchema) {
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        //15 min未更新状态则超时
+        props.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 15 * 1000 * 60 + "");
+        return new FlinkKafkaProducer<>(DEFAULT_TOPIC, serializationSchema, props, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
+    }
+
 }
